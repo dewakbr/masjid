1f644c13b00e7dceeb6b1adb36d2e2e0529c2400
pradanadewa <pradanadewa20@gmail.com>
2025-05-10 10:56:36 +0700

    update database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
 
-$autoload['libraries'] = array('session'); //,'database','Lib','pagination'
+$autoload['libraries'] = array('session','database'); //,'database','Lib','pagination'
 
 $autoload['drivers'] = array();
 
